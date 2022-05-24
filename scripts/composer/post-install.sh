03582f805240c437a588af66259f5575dacfab83
David Rodrguez <davidjguru@gmail.com>
2022-05-24 13:13:13 +0200

    #SH-7 -(BSH)-Update post-install script. #7

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -4,7 +4,7 @@
 #
 #-----------------------------------------------------------------------------------------
 # 
-# Create copies of settings and service file in a Drupal installation.
+# Create copies of settings and service files in a Drupal installation.
 # [David Rodríguez, @davidjguru] [david.rodriguez@digitalistgroup.com]
 # Title: post-install
 # Description: Copy settings and service files. 
@@ -16,12 +16,17 @@
 # https://github.com/davidjguru
 #-----------------------------------------------------------------------------------------
 #
-# Prepare the settings file for installation
+# Prepare the main basic settings file for installation
 echo "Copying default.settings.php if no settings.php exists."
 cp .settings/default.settings.php web/sites/default/settings.php
 chmod 777 web/sites/default/settings.php
 
+# Prepare the secondary settings file for installation
+echo "Copying settings.ddev.php if no settings.ddev.php exists."
+cp .settings/settings.ddev.php web/sites/default/settings.ddev.php
+chmod 777 web/sites/default/settings.ddev.php
+
 # Prepare the services file for installation
-echo "Copying services to default folder"
+echo "Copying services to default folder."
 cp .settings/default.services.yml web/sites/default/services.yml
 chmod 777 web/sites/default/services.yml
