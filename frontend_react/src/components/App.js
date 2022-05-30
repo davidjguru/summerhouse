0b9958464719036c44916c54565206d939b4a865
David Rodrguez <davidjguru@gmail.com>
2022-05-30 16:41:08 +0200

    #SH-16 -(RCT)-Initial scaffolding frontend based in ReactJS. #16

---
+++ frontend_react/src/components/App.js
@@ -0,0 +1,25 @@
+import summer_house_logo from './../summer_house_logo_512.png';
+import './../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={summer_house_logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
