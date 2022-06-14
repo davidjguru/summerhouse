7b77fc5e9daa18d418e97e5be5d2c16f4627ddea
David Rodrguez <davidjguru@gmail.com>
2022-06-14 16:58:19 +0200

    #SH-24 -(JSX)-Reorder components folders. #SH-24

--- frontend_react/src/components/App/App.js
+++ frontend_react/src/components/App/App.js
@@ -1,17 +1,18 @@
-import ArticleList from './ArticleList';
-import summer_house_logo from './../assets/summer_house_logo_512.png';
-import './../styles/App.css';
+import React from 'react';
+import Header from '../Header/Header';
+import ArticleList from '../Article/ArticleList';
+import summer_house_logo from '../../assets/summer_house_logo_512.png';
+import '../../styles/App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
         <img src={summer_house_logo} className="App-logo" alt="logo" />
         <p>Edit <code>src/App.js</code> and save to reload.</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
         <ArticleList />
-      </header>
     </div>
   );
 }
---
+++ frontend_react/src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+  
+// importing material UI components
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+  
+export default function Header() {
+  return (
+      <AppBar position="static">
+        <Toolbar>
+          {/*Inside the IconButton, we 
+           can render various icons*/}
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            {/*This is a simple Menu 
+             Icon wrapped in Icon */}
+            <MenuIcon />
+          </IconButton>
+          {/* The Typography component applies 
+           default font weights and sizes */}
+  
+          <Typography variant="h6" 
+            component="div" sx={{ flexGrow: 1 }}>
+            GeeksforGeeks Header
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+  );
+}
