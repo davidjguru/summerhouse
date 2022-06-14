e8bfd9e030dfcb1b5fb2b5a86914640f84749776
David Rodrguez <davidjguru@gmail.com>
2022-06-14 20:27:37 +0200

    #SH-24 -(JSX)-Add new component Branding for layout. #SH-24

--- frontend_react/src/components/App/App.js
+++ frontend_react/src/components/App/App.js
@@ -3,13 +3,14 @@ import Header from '../Header/Header';
 import ArticleList from '../Article/ArticleList';
 import summer_house_logo from '../../assets/summer_house_logo_512.png';
 import '../../styles/App.css';
+import Branding from '../Branding/Branding';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <img src={summer_house_logo} className="App-logo" alt="logo" />
+        <Branding />
         <p>Welcome to Summer House, these are the latest articles created in Drupal backend, obtained through a GraphQL query.</p>
         <a className="App-link" href="https://github.com/davidjguru/summerhouse/wiki" target="_blank" rel="noopener noreferrer">Learn more about Summer House</a>
         <br></br><hr></hr>
---
+++ frontend_react/src/components/Branding/Branding.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import boxImage from '../../assets/summerhouse_logo.png'
+
+// importing material UI components
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+  
+export default function Branding() {
+  return (
+    <React.Fragment>
+      <Container maxWidth="sm">
+        <Box>
+          <img src={boxImage} />
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
+}
