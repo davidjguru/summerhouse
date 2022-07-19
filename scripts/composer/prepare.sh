3f5f859a741e5c5dcfec117388b47a6e4b5c5c1b
David Rodrguez <davidjguru@gmail.com>
2022-07-19 13:54:42 +0200

    SH-40 -(CMP)-Prepare basic scaffolding for new script #40

--- scripts/composer/prepare.sh
+++ scripts/composer/prepare.sh
@@ -16,9 +16,27 @@
 # https://github.com/davidjguru
 #-----------------------------------------------------------------------------------------
 #
+# Set initial values for folders.
+DIRECTORY_1=vendor
+DIRECTORY_2=backend_drupal/modules/contrib
 # Check if the /vendor folder exists and it's populated.
-
-
+if [ -d "$DIRECTORY_1" ] && [ ! -z "$(ls -A "$DIRECTORY_1")" ] 
+  then
+    vendor=true
+else 
+  vendor=false
+fi
 # Check if the /modules/contrib exists and it's populated. 
-
+if [ -d "$DIRECTORY_2" ] && [ ! -z "$(ls -A "$DIRECTORY_2")" ] 
+  then
+    contrib=true
+else
+  contrib=false
+fi
 # Finally only launch composer install if folders doesn't exists.
+if [ "$vendor" = false ] && [ "$contrib" = false ]
+  then
+    composer install 
+else
+  echo "Folders for /modules/contrib and /vendor already exists, composer install will not be executed."
+fi
