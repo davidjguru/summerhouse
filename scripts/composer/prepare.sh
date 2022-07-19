564c59356bdf882ef5b7a13f942050483755a6ac
David Rodrguez <davidjguru@gmail.com>
2022-07-19 13:26:42 +0200

    SH-40 -(CMP)-Add new bash script for custom command #40

---
+++ scripts/composer/prepare.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+#
+#
+#-----------------------------------------------------------------------------------------
+# 
+# Review Summer House local installation to request an instal lprocess or not.
+# [David Rodríguez, @davidjguru] [david.rodriguez@digitalistgroup.com]
+# Title: prepare
+# Description: Check if the Summer House platforma was installed previously. 
+#
+# davidjguru@gmail.com
+# https://www.therussianlullaby.com
+# https://davidjguru.github.io/
+# https://dev.to/davidjguru
+# https://github.com/davidjguru
+#-----------------------------------------------------------------------------------------
+#
+# Check if the /vendor folder exists and it's populated.
+
+
+# Check if the /modules/contrib exists and it's populated. 
+
+# Finally only launch composer install if folders doesn't exists.
