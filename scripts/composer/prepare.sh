861a1bc20faa3be6d3595211bbd3fb13c1a15118
David Rodrguez <davidjguru@gmail.com>
2022-07-20 15:48:40 +0200

    SH-40 -(BSH)-Add management for some scenarios #40

--- scripts/composer/prepare.sh
+++ scripts/composer/prepare.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # -*- ENCODING: UTF-8 -*-
 #
 #
@@ -16,27 +16,50 @@
 # https://github.com/davidjguru
 #-----------------------------------------------------------------------------------------
 #
-# Set initial values for folders.
+# Set initial values for folders and variables.
 DIRECTORY_1=vendor
 DIRECTORY_2=backend_drupal/modules/contrib
+VENDOR=false CONTRIB=false INSTALLED=false
+
 # Check if the /vendor folder exists and it's populated.
 if [ -d "$DIRECTORY_1" ] && [ ! -z "$(ls -A "$DIRECTORY_1")" ] 
   then
-    vendor=true
-else 
-  vendor=false
+    VENDOR=true
 fi
+
 # Check if the /modules/contrib exists and it's populated. 
 if [ -d "$DIRECTORY_2" ] && [ ! -z "$(ls -A "$DIRECTORY_2")" ] 
   then
-    contrib=true
-else
-  contrib=false
+    CONTRIB=true
 fi
-# Finally only launch composer install if folders doesn't exists.
-if [ "$vendor" = false ] && [ "$contrib" = false ]
+
+# Check if the Drupal installation was enabled previously.
+if [ "$(drush status bootstrap)" = "Drupal Bootstrap: Successful" ]
   then
-    composer install 
+    INSTALLED=true
+fi
+
+# Finally check every possible scenario.
+if [ "$VENDOR" = true ] && [ "$CONTRIB" = true ] && [ "$INSTALLED" = true ]
+  ## First: Running Summer House after ddev stop with all resources existing.
+  then
+    echo "Folders for /modules/contrib and /vendor already exists and Drupal installation is enabled, composer install or drush will not be executed."
+elif [ "$VENDOR" = true ] && [ "$CONTRIB" = true ] && [ "$INSTALLED" = false ]
+  ## Second: Running Summer House after deletion of containers maintaining codebase.
+  then
+    echo "Folders for /modules/contrib and /vendor already exists but Drupal installation is not enabled, drush site:install will be executed."
+    drush site-install --existing-config --site-name=SummerHouse --account-name=admin --account-pass=admin --db-url=mysql://db:db@db/db -y
+    drush user-add-role 'administrator' admin
+    yes |drush cset system.site uuid $(head -n 1 ./.settings/salt.txt)
+    drush cr
+    drush genc --bundles=article 10
 else
-  echo "Folders for /modules/contrib and /vendor already exists, composer install will not be executed."
+  ## Third: First time Installation running ddev start.
+  echo "Folders for /modules/contrib and /vendor doesn't exists and your Drupal installation is not enabled, composer install and drush will be executed."
+  composer install 
+  drush site-install --existing-config --site-name=SummerHouse --account-name=admin --account-pass=admin --db-url=mysql://db:db@db/db -y
+  drush user-add-role 'administrator' admin
+  yes |drush cset system.site uuid $(head -n 1 ./.settings/salt.txt)
+  drush cr
+  drush genc --bundles=article 10
 fi
