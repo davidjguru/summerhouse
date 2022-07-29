f9bb03e32f383176725c800ce1e3d0ac6df3e53c
David Rodrguez <davidjguru@gmail.com>
2022-07-29 12:56:47 +0200

    SH-41 -(RCT)-Remove errors due to using React 18 and Webpack 5
    Read More:
      https://alchemy.com/blog/how-to-polyfill-node-core-modules-in-webpack-5
      https://github.com/react-dnd/react-dnd/issues/3425

--- frontend_react/src/components/Chapter/ChapterItem.jsx
+++ frontend_react/src/components/Chapter/ChapterItem.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-
+import React, { useEffect, useState} from 'react';
+import { getCharacters } from 'rickmortyapi';
+ 
+async function loadResources() {
+  const aliveRicks = await getCharacters({
+    name: 'rick',
+    status: 'alive'
+  });
+  return aliveRicks;
+}
 export function ChapterItem({ id, title }) {
-  
+const loading = loadResources();
+console.log(loading);
+const imageSource = "https://picsum.photos/200";
   return (
     <li>
-      <input type="checkbox" />
-      {id} - {title}
+      <div className="card">
+        <img src={imageSource} alt="Avatar" />
+          <div className="container">
+            <h4><b>{id}</b></h4>
+            <p>{title}</p>
+          </div>
+      </div> 
     </li>
   );
 }
